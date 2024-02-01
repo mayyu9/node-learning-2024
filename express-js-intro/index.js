998cd46b1050c61f5924b794a4f1b29a63cc309c
mayyu9 <coolmahimsd@gmail.com>
2024-02-01 22:44:51 +0530

    Middlewares implementation

--- express-js-intro/index.js
+++ express-js-intro/index.js
@@ -4,6 +4,12 @@ const app = express();
 
 const port  = 8000;
 
+// middle ware
+app.use((req, res, next) => {
+    console.log(`[${new Date().toLocaleString()}] ${req.method}: ${req.url}`);
+    next(); // this will process the req futher if any exists. or to the respected url.
+})
+
 app.get('/', (req, res) => {
     return res.json({message: 'Hello from express server.'})
 })
